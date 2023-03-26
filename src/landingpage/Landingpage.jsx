e7983034dcebb532cd49844ca8bd852865f77d7c
kulotzkie980@gmail.com <kulotzkie980@gmail.com>
2023-03-27 00:04:45 +0800

    add details

--- src/landingpage/Landingpage.jsx
+++ src/landingpage/Landingpage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from './Hero'
 import Services from './Services'
+import Products from './Products'
 import About from './About'
 import Spacer from './Spacer'
 import Contact from './Contact'
@@ -10,6 +11,7 @@ const Landingpage = () => {
     <>
         <Hero/>
         <Services/>
+        <Products/>
         <About/>
         <Spacer/>
         <Contact/>
